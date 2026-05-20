7c3e67ffd0109bf9b9cdcc4ef07f5da6a38c5f06
gpreth <preethibabu1998@gmail.com>
2026-05-20 17:39:14 +0530

    Add files via upload

---
+++ arrayha.js
@@ -0,0 +1,10 @@
+let word = [4,3,32,'hello']
+console.log(word.length) //length of array
+console.log(word.push(3)) //push 3 at last index
+console.log(word)
+console.log(word.pop(4)) //delete last in index by pop
+console.log(word)
+console.log(word.unshift("hi")) //insert word in frst index
+console.log(word)
+console.log(word.shift("hi")) //delete word in frst index
+console.log(word)
